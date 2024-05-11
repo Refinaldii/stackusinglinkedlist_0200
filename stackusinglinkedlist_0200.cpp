0f842611a46f133fdeea9c8d31b12679ce863671
Refinaldii <refinaldi9786@gmail.com>
2024-05-11 10:07:41 +0700

    mengisi bagian dari int push

--- stackusinglinkedlist_0200.cpp
+++ stackusinglinkedlist_0200.cpp
@@ -25,7 +25,12 @@ public :
 	}
 	int push(int value)
 	{
-
+		Node* newNode = new Node();
+		newNode->data = value;
+		newNode->next = top;
+		top = newNode;
+		cout << "Push value: " << value << endl;
+		return value;
 	}
 };
 int main()
